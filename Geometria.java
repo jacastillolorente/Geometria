0c4eb95b5c1d65b7e0ba547b33d9f2a0559c2da1
Juan Andres <[j.acastillolorente@gmail.com]>
2017-11-02 13:25:14 +0100

    Commit inicial que contiene dos métodos para calcular áreas de cuadrados y rectángulos

---
+++ Geometria.java
@@ -0,0 +1,8 @@
+public class Geometria {
+	public static double calcularAreaCuadrado (double lado) {
+		return lado * lado;
+	}
+	public static double calcularAreaRectangulo (double base, double altura) {
+		return base * altura;
+	}
+}
